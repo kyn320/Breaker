2ef48e9c94778682545be341af1c5216e7cd3a05
kyn320@naver.com <kyn320@naver.com>
2018-06-01 21:29:47 +0900

    gun aniamtion test

---
+++ Assets/Scripts/SortingLayerSprite.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingLayerSprite : MonoBehaviour
+{
+    public enum Category
+    {
+        PositionX,
+        PositionY
+    }
+
+    Transform tr;
+    SpriteRenderer spriteRenderer;
+
+    public Category category;
+    public int sortDir = 1;
+
+    void Awake() {
+        tr = GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(category) {
+            case Category.PositionX:
+                spriteRenderer.sortingOrder = (int)(tr.position.x * 10) * sortDir;
+                break;
+            case Category.PositionY:
+                spriteRenderer.sortingOrder = (int)(tr.position.y * 10) * sortDir;
+                break;
+        }
+    }
+}
+
